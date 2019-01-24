c773bb26416245b16e232843162fbf7289153990
Mohammed Ali Eslami <meslami@netrias.com>
2019-01-24 10:57:49 -0500

    updated feature analysis to include the live dead control and drop the class label

--- src/pysd2cat/analysis/feature_analysis.py
+++ src/pysd2cat/analysis/feature_analysis.py
@@ -134,7 +134,8 @@ def main():
 
     print("Building Live/Dead Control Dataframe...")
     #live_dead_df = pipeline.get_flow_dataframe(data_dir,filename="WT-Dead-Control__.fcs")
-    live_dead_df = pipeline.get_dataframe_for_live_dead_classifier(data_dir,fraction=.01,max_records=1000)
+    live_dead_df = pipeline.get_dataframe_for_live_dead_classifier(data_dir,control_type=[Names.WT_DEAD_CONTROL],fraction=.01,max_records=1000)
+    live_dead_df = live_dead_df.drop(columns=['class_label'])
     nrows = len(live_dead_df)
     ncols = len(live_dead_df.columns)
     print("Dataframe constructed with {0} rows and {1} columns".format(nrows,ncols))
