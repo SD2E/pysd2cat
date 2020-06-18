d77a6a8c22e45bfc9bab02881a9b5c58ee5d99fe
Hamed <eramian@netrias.com>
2020-06-18 17:11:14 -0400

    Created pipeline_tester skeleton and minor changes - pipeline_tester will consistently test LiveDeadPipeline class for errors

--- hamed_live_dead_analysis/pipeline_tester.py
+++ hamed_live_dead_analysis/pipeline_tester.py
@@ -3,7 +3,7 @@ import sys
 import matplotlib
 import numpy as np
 import pandas as pd
-import seaborn as sns
+from hamed_live_dead_analysis.names import Names
 from hamed_live_dead_analysis.pipeline_class import LiveDeadPipeline
 from hamed_live_dead_analysis.names import Names as n
 
@@ -14,13 +14,7 @@ pd.set_option('display.max_colwidth', -1)
 
 
 def main():
-    ldp = LiveDeadPipeline(x_strain=n.yeast, x_treatment=n.ethanol, x_stain=1,
-                           y_strain=None, y_treatment=None, y_stain=1)
-    ldp.load_data()
-    # ldp.plot_distribution()
-    # ldp.condition_method()
-    ldp.thresholding_method()
-    ldp.evaluate_performance()
+    pass
 
 
 if __name__ == '__main__':
