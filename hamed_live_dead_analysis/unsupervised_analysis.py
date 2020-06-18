032dabce2eb5ab7544a542e6b674e543642c83b5
Hamed <eramian@netrias.com>
2020-06-18 09:20:36 -0400

    Added and ran cluster-driven labeling and tuple-driven labeling - Both of these will be moved to LiveDeadPipeline (already moved 1)

--- hamed_live_dead_analysis/unsupervised_analysis.py
+++ hamed_live_dead_analysis/unsupervised_analysis.py
@@ -7,6 +7,11 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.cluster import KMeans, MeanShift
+from harness.test_harness_class import TestHarness
+from harness.th_model_instances.hamed_models.random_forest_classification import random_forest_classification
+import matplotlib
+
+matplotlib.use("tkagg")
 
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 10000)
@@ -35,6 +40,7 @@ def main():
 
     # set stain here. 0 = don't use stains, 1 = use stains
     stain = 1
+    print("stain = {}".format(stain))
 
     if stain == 0:
         fcols_yeast = yeast_features_0
@@ -46,7 +52,6 @@ def main():
         ecoli_train_bank = ecoli_train_bank.loc[ecoli_train_bank["stain"] == 0]
         ecoli_test_df = ecoli_test_df.loc[ecoli_test_df["stain"] == 0]
     elif stain == 1:
-        print("whoohoo!")
         fcols_yeast = yeast_features_1
         fcols_basc_ecoli = basc_ecoli_features_1
         yeast_train_bank = yeast_train_bank.loc[yeast_train_bank["stain"] == 1]
@@ -58,7 +63,7 @@ def main():
     else:
         raise NotImplementedError()
 
-    organism = "ecoli"
+    organism = "yeast"
 
     if organism == "yeast":
         train_df = yeast_train_bank
@@ -75,8 +80,8 @@ def main():
     else:
         raise NotImplementedError()
 
-    n_clusters = 3
-    c_model = KMeans(n_clusters=n_clusters)
+    n_clusters = 4
+    c_model = KMeans(n_clusters=n_clusters, random_state=5)
     c_model.fit(train_df[feature_cols])
     test_df["cluster_preds"] = c_model.predict(test_df[feature_cols])
 
@@ -89,7 +94,71 @@ def main():
 
     sns.heatmap(frequency_table, cmap="Blues")
     plt.title("{}: Ethanol Concentration vs. {} KMeans Clusters.".format(organism, n_clusters))
-    plt.show()
+    # plt.show()
+
+    # TODO: right now I have clustering with a train/test split, and then I'm doing another train/test split on the test set...
+    # ---------------------------------- cluster-driven labeling and model run ----------------------------------
+    # add labels based on cluster results
+    # labeled_df = test_df.copy()
+    # labeled_df["label"] = -1  # super-dead
+    # labeled_df.loc[labeled_df["cluster_preds"] == 0, "label"] = 0  # dead
+    # labeled_df.loc[labeled_df["cluster_preds"] == 3, "label"] = 1  # live
+    #
+    # print(labeled_df["label"].value_counts(dropna=False))
+    # print()
+    # labeled_df = labeled_df.loc[labeled_df["label"] != -1]
+    # print(labeled_df["label"].value_counts(dropna=False))
+    # print()
+    # print(labeled_df)
+    #
+    # labeled_train, labeled_test = train_test_split(labeled_df, train_size=0.7, random_state=5,
+    #                                                stratify=labeled_df[["ethanol", "time_point", "label"]])
+    # print("Shape of labeled_train: {}".format(labeled_train.shape))
+    # print("Shape of labeled_test: {}".format(labeled_test.shape))
+    #
+    # current_path = os.getcwd()
+    # output_path = os.path.join(current_path, "ld_definition_results")
+    # print("initializing TestHarness object with output_location equal to {} \n".format(output_path))
+    # th = TestHarness(output_location=output_path)
+    # col_to_predict = "label"
+    # th.run_custom(function_that_returns_TH_model=random_forest_classification, dict_of_function_parameters={},
+    #               training_data=labeled_train, testing_data=labeled_test,
+    #               data_and_split_description="Cluster-driven labels",
+    #               cols_to_predict=col_to_predict, feature_cols_to_use=yeast_features_1,
+    #               index_cols=["arbitrary_index", "ethanol", "time_point"], normalize=False, feature_cols_to_normalize=yeast_features_1,
+    #               feature_extraction="eli5_permutation", predict_untested_data=False)
+
+    # ---------------------------------- treatment-driven labeling and model run ----------------------------------
+    treatment_df = test_df.copy()
+    print(treatment_df)
+    print()
+    treatment_labeled = treatment_df.loc[((treatment_df["ethanol"] == 0.0) | (treatment_df["ethanol"] == 1120.0))
+                                         & (treatment_df["time_point"] == 12)]
+    # treatment_unlabled = treatment_df.loc[~(((treatment_df["ethanol"] == 0.0) | (treatment_df["ethanol"] == 1120.0))
+    #                                         & (treatment_df["time_point"] == 12))]
+
+    treatment_labeled.loc[treatment_labeled["ethanol"] == 0.0, "label"] = 1
+    treatment_labeled.loc[treatment_labeled["ethanol"] == 1120.0, "label"] = 0
+    print(treatment_labeled)
+    print()
+    print(treatment_labeled["label"].value_counts(dropna=False))
+    print()
+
+    current_path = os.getcwd()
+    output_path = os.path.join(current_path, "ld_definition_results")
+    print("initializing TestHarness object with output_location equal to {} \n".format(output_path))
+    th = TestHarness(output_location=output_path)
+    col_to_predict = "label"
+    th.run_custom(function_that_returns_TH_model=random_forest_classification, dict_of_function_parameters={},
+                  training_data=treatment_labeled, testing_data=treatment_labeled,
+                  data_and_split_description="Treatment-driven labels",
+                  cols_to_predict=col_to_predict, feature_cols_to_use=yeast_features_1,
+                  index_cols=["arbitrary_index", "ethanol", "time_point"], normalize=False, feature_cols_to_normalize=yeast_features_1,
+                  feature_extraction="eli5_permutation", predict_untested_data=treatment_df)
+
+    # ---------------------------------- stain-driven labeling and model run ----------------------------------
+    # stain_df = test_df.copy()
+    # print(stain_df)
 
 
 if __name__ == '__main__':
