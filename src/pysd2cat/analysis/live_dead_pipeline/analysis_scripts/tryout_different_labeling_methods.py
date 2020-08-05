a988041dea5d9a6f9c5f523626b9b7b6a63a1ecf
Hamed <eramian@netrias.com>
2020-08-05 12:02:42 -0400

    Cleaned up scatterplot grid code and got it to a functional state now - plot_features_over_conditions method inside of LiveDeadPipeline works   correctly, and it calls grid_of_distributions_over_conditions - compare_plots_of_features_over_conditions method inside of   ComparePipelines works correctly, and it calls   grid_of_distributions_over_conditions - saving plotly figures as only html files for now, because having   issues with saving them as static figures. Will add static image   saving in the future, but it's not a huge deal for now. - moved master_script.py to analysis_scripts folder, and renamed it   to tryout_different_labeling_methods.py, because we will be doing   different analyses at different times, so there's no need for a   "master_script" - created tryout_compare_pipelines.py, which is supposed to test out   the methods of ComparePipelines. Currently it's testing the   grid_of_distributions_over_conditions function through calls to both   LiveDeadPipeline and ComparePipelines.

---
+++ src/pysd2cat/analysis/live_dead_pipeline/analysis_scripts/tryout_different_labeling_methods.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import matplotlib
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from pysd2cat.analysis.live_dead_pipeline.ld_pipeline_classes import LiveDeadPipeline, ComparePipelines
+from pysd2cat.analysis.live_dead_pipeline.names import Names as n
+
+matplotlib.use("tkagg")
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 10000)
+pd.set_option('display.max_colwidth', None)
+
+
+def main():
+    ldp = LiveDeadPipeline(x_strain=n.yeast, x_treatment=n.ethanol, x_stain=0,
+                           y_strain=None, y_treatment=None, y_stain=None)
+    ldp.load_data()
+    ldp.plot_distribution(channel=n.sytox_cols[0])
+    ldp.thresholding_method()
+    ldp.condition_method(live_conditions=None,
+                         dead_conditions=[
+                             {n.ethanol: 1120.0, n.time: n.timepoints[-1]},
+                             {n.ethanol: 280.0, n.time: n.timepoints[-1]}
+                         ]
+                         )
+    ldp.cluster_method(n_clusters=2)
+    ldp.evaluate_performance(n.thresholding_method)
+    ldp.evaluate_performance(n.condition_method)
+    ldp.evaluate_performance(n.cluster_method)
+    ldp.plot_percent_live_over_conditions(n.condition_method)
+    ldp.plot_features_over_conditions(n.thresholding_method, kdeplot=True)
+
+
+if __name__ == '__main__':
+    main()
